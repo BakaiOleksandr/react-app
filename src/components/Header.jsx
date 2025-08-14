072bec8a368f06fcff4c17d0631a5e20e64b93cf
Oleksandr Bakai <saniabakay@gmail.com>
2025-08-14 03:02:28 +0200

    add Header component and removed nested content

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="Header">
+      <h2>Hello React!</h2>
+      <img
+        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
+        alt="logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
